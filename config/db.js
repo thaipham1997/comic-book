36db0bbddd6296aa9153f62896e2a3ee7dd0c4c4
GTS-IT-GROUP-2018 <gtsit.secure@gmail.com>
2018-06-22 18:27:10 +0700

    haha

--- config/db.js
+++ config/db.js
@@ -3,5 +3,5 @@ module.exports = {
     dbConnect: dbConnect
 }
 function dbConnect() {
-    mongoose.connect('mongodb://localhost:27017/comic-book');
+    mongoose.connect('mongodb://phamhongthai:hongthai11@ds117101.mlab.com:17101/comic-book');
 }
